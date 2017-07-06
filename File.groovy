5d68e8680291a909af7b283d4574998082b088aa
aion3181 <aion3181@gmail.com>
2017-07-06 23:13:28 +0300

    push half of script

--- File.groovy
+++ File.groovy
@@ -1,48 +1,70 @@
 /**
  * Created by USER on 05.07.2017.
  */
-//CliBuilder cli = new CliBuilder(
-//        usage: 'groovy File.groovy -u {user} -p {password} -d {path to content} -n {repoName} [-h {nx3Url}]')
-//cli.with {
-//    u longOpt: 'username', args: 1, required: true, 'User with permissions to deploy to the target repo'
-//   p longOpt: 'password', args: 1, required: true, 'Password for user'
-//   r longOpt: 'repository', args: 1, required: true, 'Name of raw repository to deploy to, strict content validation is suggested to be turned off.'
-//    d longOpt: 'directory', args: 1, required: true, 'Path of file to dploy'
-//    h longOpt: 'host', args: 1, 'Nexus Repository Manager 3 host url (including port if necessary). Defaults to http://nexus/repository/Artifact_storage/'
-//}
-//def options = cli.parse(args)
-//if (!options) {
-//    return
-//}
+@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.2')
+import groovyx.net.http.HTTPBuilder
+
+/**
+ * download|upload file to Nexus 3.x. raw repo
+ */
+import org.apache.http.HttpRequest
+import org.apache.http.HttpRequestInterceptor
+import org.apache.http.protocol.HttpContext
+import org.apache.tools.ant.taskdefs.GZip
+
+import static groovy.io.FileType.FILES
+import static groovyx.net.http.ContentType.TEXT
+import static groovyx.net.http.Method.PUT
+
+CliBuilder cli = new CliBuilder(
+        usage: 'groovy file.groovy -e {pull|push} -u {user} -p {password} -d {path to content} -f {name of file} -n {repoName} [-h {nx3Url}]')
+cli.with {
+    e longOpt: 'execute', args: 1,  required: true, values: ['pull','push'], 'pull for download | push for upload'
+    u longOpt: 'username', args: 1, 'User with permissions to upload to the target repo'
+    p longOpt: 'password', args: 1, 'Password for user'
+    r longOpt: 'repository', args: 1, 'Name of raw repository to upload to | download from , strict content validation is suggested to be turned off.'
+    d longOpt: 'directory', args: 1, required: true, 'Path of directory with artifact to upload or where to download artifact'
+    f longOpt: 'filename', args: 1, required: true, 'name of artifact to upload|download'
+    h longOpt: 'host', args: 1, 'Nexus Repository Manager 3 host url (including port if necessary). Defaults to http://nexus/'
+}
+def options = cli.parse(args)
+if (!options) {
+    return
+}
+
+File sourceFolder = new File(options.d ?: '/home/student/Downloads/nexus/')
+assert sourceFolder.exists(): "${sourceFolder} does not exist"
+def username = (options.u ?: 'admin')
+def password = (options.password ?: 'admin123')
+def authInterceptor = new HttpRequestInterceptor() {
+    void process(HttpRequest httpRequest, HttpContext httpContext) {
+        httpRequest.addHeader('Authorization', 'Basic ' + "${username}:${password}".bytes.encodeBase64().toString())
+    }
+}
+def choice = (options.e)
+HTTPBuilder http = new HTTPBuilder(options.h ?: 'http://nexus/')
+http.client.addRequestInterceptor(authInterceptor)
+def resourcePath = "/repository/${options.r ?: 'Artifact_storage2'}/"
+
+def file = (options.f ?: 'helloworld-15.tar.gz')
+
+        if($choice=="push"){
+            println "pushing $file"
+            http.request(PUT) {
+                uri.path = "$resourcePath$ + $file"
+                requestContentType = TEXT
 
 
-def command1 = "curl  -v  --user 'jenkinsnexus:jenkins' " +
-        "--upload-file  helloworld-$BUILD_NUMBER.tar.gz   " +
-        "http://nexus/repository/Artifact_storage/helloworld-$BUILD_NUMBER.tar.gz"
-println command1
-def proc1 = command1.execute()
-proc1.waitFor()
-
-println "return code: ${proc1.exitValue()}"
-println "stderr: ${proc1.err.text}"
-println "stdout: ${proc1.in.text}"
-
-
-def command2 = '''/usr/bin/ssh -p vagrant vagrant@192.168.56.26 bash -s <<-SHELL
-
-wget http://jenkinsnexus:jenkins@nexus/repository/Artifact_storage/$PRNUMBER
-    tar -xzf $PRNUMBER
-    rm $PRNUMBER
-    cd /opt/jboss/wildfly-10.1.0.Final/standalone/deployments/
-    if [ -f "helloworld.war" ]
-    then
-    mv helloworld.war /tmp/helloworld.war.old-$BUILD_NUMBER
-    fi
-    mv /root/helloworld.war /opt/jboss/wildfly-10.1.0.Final/standalone/deployments/helloworld.war
-        SHELL '''
-println command2
-def proc2 = command2.execute()
-proc1.waitFor()
-println "return code: ${proc2.exitValue()}"
-println "stderr: ${proc2.err.text}"
-println "stdout: ${proc2.in.text}"
+                body = file.text
+                response.success = { resp ->
+                    println "POST response status: ${resp.statusLine}"
+                    assert resp.statusLine.statusCode == 201
+                }
+            }
+            
+                return file.getPath()
+            
+        }else {
+println 'pull'
+            
+        }
