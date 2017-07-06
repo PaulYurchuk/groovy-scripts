0dcc3f717e6e8711ca45a36a4da3fd5e931b9296
PaulYurchuk <paulyurchuk@gmail.com>
2017-07-06 22:38:38 +0300

    Groovy script for list of existing artifacts in Nexus

--- artifactList.groovy
+++ artifactList.groovy
@@ -1,7 +1,9 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1' )
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
 
+def listArtifacts = []
 def httpBuilder = new HTTPBuilder("http://nexus")
 def httpRequest =  """ { "action": 
 "coreui_Component",    
@@ -16,19 +18,30 @@ def httpRequest =  """ { "action":
 "tid":15	
 }"""
 
-request(POST, TEXT) { req ->
-  uri.path = "/service/extdirect"
-  headers."Content-Type" = "application/json"
-  headers.'Accept' = "*/*"
-  headers.Accept = "application/json"
-  body = httpRequest
-  headers.'Authorization' = "Basic ${"admin:123".bytes.encodeBase64().toString()}"
+    httpBuilder.request(POST, TEXT) { req ->
+    uri.path = "/service/extdirect"
+    headers."Content-Type" = "application/json"
+    headers.'Accept' = "*/*"
+    headers.Accept = "application/json"
+    body = httpRequest
+    headers.'Authorization' = "Basic ${"admin:123".bytes.encodeBase64().toString()}"
 
-  response.success = { resp, ->
-  println "Status: OK. Status code:  " + "\n" + "${resp.status}"
-  }
-  
-  response.failure = { resp, ->
-  println "Status: Failure. Status code:  " + "\n" + "${resp.status}"
-  }
+    response.success = { resp, json ->
+        println "Status: OK" + "\n" + "Status code: ${resp.status}"
+
+        def jsonSlurper = new groovy.json.JsonSlurper()
+        def jsonResponse = json.text as String
+        def parseResponse = jsonSlurper.parseText(jsonResponse)
+
+        parseResponse.result.data.each {
+            if (it.name.matches(~/.+.tar.gz/)) {
+                listArtifacts.add(it.name)
+            }
+        }
+    }
+
+    response.failure = { resp ->
+        println "Status: Failure. Status code:  " + "\n" + "${resp.status}"
+    }
 }
+    println listArtifacts
