b9dba68e4d9ed191e81e9542f4fd11a15fa7d135
bubalush <bubalush@gmail.com>
2017-07-07 17:52:35 +0300

    Update upload.groovy

--- upload.groovy
+++ upload.groovy
@@ -17,7 +17,7 @@ def cred = "nexus-service-user:jenkins"
 def artifact = "hello-59-2.0-release.tar.gz"
 def repo = "project-releases"
 def way = "http://192.168.50.11:8081/"
-def File = new File ("${ARTIFACT_SUFFIX}-{$BUILD_NUMBER}.tar.gz").getBytes()
+def File = new File ("${ARTIFACT_SUFFIX}-${BUILD_NUMBER}.tar.gz").getBytes()
 
 def connection = new URL( "${way}/repository/${repo}/${ARTIFACT_SUFFIX}/${ARTIFACT_SUFFIX}/2.0/${ARTIFACT_SUFFIX}-{$BUILD_NUMBER}.tar.gz" )
         .openConnection() as HttpURLConnection
