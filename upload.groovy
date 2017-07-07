159001bccc39b0ac63f0b156e90fe4b0a9cf8bb4
bubalush <bubalush@gmail.com>
2017-07-07 17:32:07 +0300

    Update upload.groovy

--- upload.groovy
+++ upload.groovy
@@ -1,11 +1,19 @@
-def cred = "admin:admin123"
-def artifact = "hello-59-2.0-release.tar.gz"
-def local = "/home/student/"
-def repo = "task8"
-//def way = "http://nexus/repository/${repo}/helloworld/hello-59/2.0/"
-def File = new File ("/home/student/hello-59-2.0-release.tar.gz").getBytes()
+class upload {
 
-def connection = new URL( "http://nexus/repository/${repo}/helloworld/hello-59/2.0/hello-59-2.0-release.tar.gz" )
+    public static void main(def args) {
+        println("Printing arguments");
+        for(String arguments : args) {
+            println (arguments);
+        }
+    }
+
+def cred = "nexus-service-user:jenkins"
+def artifact = "hello-59-2.0-release.tar.gz"
+def repo = "project-releases"
+def way = "http://192.168.50.11:8081/"
+def File = new File ("${ARTIFACT_SUFFIX}-{$BUILD_NUMBER}.tar.gz").getBytes()
+
+def connection = new URL( "${way}/repository/${repo}/${ARTIFACT_SUFFIX}/${ARTIFACT_SUFFIX}/2.0/${ARTIFACT_SUFFIX}-{$BUILD_NUMBER}.tar.gz" )
         .openConnection() as HttpURLConnection
 def auth = "${cred}".getBytes().encodeBase64().toString()
 
