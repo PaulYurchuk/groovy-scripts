94e68aa8840f2caa5a7c901f1d2dc6b041dccac7
bubalush <bubalush@gmail.com>
2017-07-08 01:18:57 +0300

    Update sftp.groovy

--- sftp.groovy
+++ sftp.groovy
@@ -4,7 +4,7 @@ import com.jcraft.jsch.*
 java.util.Properties config = new java.util.Properties()
 config.put "StrictHostKeyChecking", "no"
 JSch ssh = new JSch()
-Session sess = ssh.getSession "root", "nexus", 22
+Session sess = ssh.getSession "root", "192.168.50.12", 22
 sess.with {
     setConfig config
     setPassword "vagrant"
@@ -12,8 +12,8 @@ sess.with {
     Channel chan = openChannel "sftp"
     chan.connect()
     ChannelSftp sftp = (ChannelSftp) chan;
-    def sessionsFile = new File('/home/student/hello-58-2.0-release.tar.gz')
-    sessionsFile.withInputStream { istream -> sftp.put(istream, "/home/hello-58-2.0-release.tar.gz") }
+    def sessionsFile = new File('/opt/jenkins/master/workspace/Task8test/artifact.tar.gz')
+    sessionsFile.withInputStream { istream -> sftp.put(istream, "/home/artifact.tar.gz") }
     chan.disconnect()
     disconnect()
 }
