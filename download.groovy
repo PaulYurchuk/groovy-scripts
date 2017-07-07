1efbf8cc5732de4cf1fff14edab4c55148d54048
bubalush <bubalush@gmail.com>
2017-07-07 18:51:27 +0300

    Update download.groovy

--- download.groovy
+++ download.groovy
@@ -1,10 +1,22 @@
-def cred = "admin:admin123"
-def artifact = "hello-58-2.0-release.tar.gz"
-def local = "/home/student/"
-def repo = "task8"
-def way = "http://nexus/repository/${repo}/helloworld/hello-58/2.0/"
-new File("${local}${artifact}").withOutputStream { out ->
-    def url = new URL("${way}${artifact}").openConnection()
+CliBuilder cli = new CliBuilder(
+    usage: 'groovy upload.groovy -a {ARTIFACT_NAME}')
+cli.with {
+  a longOpt: 'ARTIFACT_NAME', args: 1, required: true, 'ARTIFACT_NAME from job Jenkins'
+  }
+def options = cli.parse(args)
+if (!options) {
+  return
+}
+
+def  ARTIFACT_NAME= options.a
+
+def cred = "nexus-service-user:jenkins"
+def repo = "project-releases"
+def way = "http://192.168.50.11:8081"
+
+def way = "${way}/repository/${repo}/helloworld/18/2.0/"
+new File("${ARTIFACT_NAME}").withOutputStream { out ->
+    def url = new URL("${way}${ARTIFACT_NAME}").openConnection()
     def remoteAuth = "Basic " + "${cred}".bytes.encodeBase64()
     url.setRequestProperty("Authorization", remoteAuth);
     out << url.inputStream
