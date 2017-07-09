c96a6af149201e89b590715b68432908f067fc07
zvirinsky <virinsky@gmail.com>
2017-07-09 10:05:01 +0300

    fail /adding vars, extension error

--- download_artf.groovy
+++ download_artf.groovy
@@ -1,4 +1,14 @@
-def url = 'http://www.google.com/images/logo.gif'
-def file = new File('google_logo.gif').newOutputStream()
+def ARTIFACTID = "zvirinsky"
+def BUILD_NUMBER = "23"
+def GROUPID = "Task-8"
+def VERSIONID = "1.0"
+def baseURL = "http://192.168.1.223:8081"
+def repositoryid = "MNT-maven2-hosted-releases"
+def asd = "tar.gz"
+
+
+
+def url = "${baseURL}/repository/${repositoryid}/${GROUPID}/${ARTIFACTID}_${BUILD_NUMBER}/${VERSIONID}/"
+def file = new File("${ARTIFACTID}_${BUILD_NUMBER}-${VERSIONID}.${asd}").newOutputStream()
 file << new URL(url).openStream()
 file.close()
