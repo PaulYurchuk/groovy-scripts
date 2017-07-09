00463ddd09769cb364a47eb819271ffd3e004fbb
SVLay <ssvlay@gmail.com>
2017-07-09 18:40:04 +0300

    complete rework on code, used RPN transofrmation(sort station), infix form is much easier to parse and compute

--- calculator.groovy
+++ calculator.groovy
@@ -1,58 +1,125 @@
-import jdk.nashorn.internal.runtime.regexp.joni.Regex
-import org.apache.tools.ant.util.regexp.Regexp
-import org.codehaus.groovy.ant.Groovy
-
-import java.util.regex.Matcher
-
 /**
- * Created by student on 7/6/17.
+ * Created by student on 7/7/17.
  */
 
 
-//add brackets support
-def ainput = "32 / 8 - 48 + 540 + 6 * 2"
-def operators = ['/','*','-','+']
+enum Operator {
+    PLUS('+', 0, { a, b -> a + b }),
+    MINUS('-', 0, { a, b -> a - b }),
+    MULTIPLY('*', 1, { a, b -> a * b }),
+    DIVIDE('/', 1, { a, b -> a / b })
+    String value
+    int precedence
+    Closure calculate
 
-print ainput + " = "
-
-class inputParserClass {
-    String[] parsedInput
-    inputParserClass(String input) {
-        parsedInput = input.findAll("(\\d+\\.?\\d*|[\\/\\*\\-\\+\\(\\)])")
+    private Operator(String value, int precedence, Closure calculate) {
+        this.value = value
+        this.precedence = precedence
+        this.calculate = calculate
     }
 
+    def static valueFrom(string) {
+        for (operator in Operator.values()) {
+            if (operator.value == string) {
+                return operator
+            }
+        }
+        throw new IllegalArgumentException("invalid operator ${string}")
+    }
+
+    def int compareTo(other) {
+        precedence <=> other.precedence
+    }
+
+    def String toString() {
+        value
+    }
 }
-class atomicOperation {
-    String performAtomicOperation(String leftArgument, String rightArgument, String operation) {
-        switch (operation[0]){
-            case '+' :
-                return (leftArgument.toDouble() + rightArgument.toDouble()).toString()
-                break
-            case '-' :
-                return (leftArgument.toDouble() - rightArgument.toDouble()).toString()
-                break
-            case '/' :
-                //to do: division by zero check
-                return (leftArgument.toDouble() / rightArgument.toDouble()).toString()
-                break
-            case '*' :
-                return (leftArgument.toDouble() * rightArgument.toDouble()).toString()
-                break
+
+class sortStation {
+    def static convertToPostfix(infixExpression) {
+        def output = []
+        def stack = []
+        tokenizeProcess(infixExpression).split().each { token ->
+            processToken(token, output, stack)
+        }
+        while (!stack.isEmpty()) {
+            output.add stack.pop()
+        }
+        output.join(' ')
+    }
+
+    def static processToken(token, output, stack) {
+        if (token.isNumber()) {
+            output.add token
+        } else {
+            switch (token) {
+                case "(":
+                    stack.add token
+                    break
+                case ")":
+                    while (stack.last() != "(") {
+                        output.add stack.pop()
+                    }
+                    stack.pop()
+                    break
+                default:
+                    def operator = Operator.valueFrom(token)
+                    while (!stack.isEmpty() &&
+                            stack.last().is(Operator) &&
+                            operator <= stack.last()) {
+                        output.add stack.pop()
+                    }
+                    stack.add operator
+            }
         }
     }
+
+    def static tokenizeProcess(string) {
+        def output = []
+        string.each { letter ->
+            if (!output.isEmpty() && letter != ' ') {
+                def last = output.last()
+                if (isParen(letter) && last != ' ') {
+                    output.add ' '
+                } else if (isParen(last)) {
+                    output.add ' '
+                }
+            }
+            output.add letter
+        }
+        output.join()
+    }
+
+    def static isParen(letter) {
+        letter == '(' || letter == ')'
+    }
 }
 
-
-def calc = new atomicOperation()
-def input = new inputParserClass(ainput)
-
-
-while(input.parsedInput.findIndexOf {it in operators} > -1 ) {
-    def  currentOperationIndex =  input.parsedInput.findIndexOf {it in operators}
-    input.parsedInput[currentOperationIndex] = calc.performAtomicOperation(input.parsedInput[currentOperationIndex - 1],input.parsedInput[currentOperationIndex + 1], input.parsedInput[currentOperationIndex])
-    input.parsedInput = input.parsedInput.minus(input.parsedInput[currentOperationIndex - 1], input.parsedInput[currentOperationIndex + 1])
-
+def computePostfix(postfixExpression) {
+    def stack = []
+    postfixExpression.split().each { token ->
+        if (token.isNumber()) {
+            stack.add(token)
+        } else {
+            def b = stack.pop().toBigDecimal()
+            def a = stack.pop().toBigDecimal()
+            stack.add(Operator.valueFrom(token).calculate(a, b))
+        }
+    }
+    stack.pop()
 }
 
-print(input.parsedInput)
+def computeInfix(infixExpression) {
+    computePostfix(sortStation.convertToPostfix(infixExpression))
+}
+
+def infixExpression = '5.5 + ((6 + 2) * 4) - -3 * 1.5'
+def expectedResult = 42
+assert computeInfix(infixExpression) == expectedResult
+
+
+
+
+
 
