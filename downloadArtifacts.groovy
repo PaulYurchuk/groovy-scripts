2a02b7e1418bd45f329fd6f9829cdc0b448fc632
Andrei Konchyts <a.konchic@gmail.com>
2017-07-09 15:29:27 +0300

    download script beta version

--- downloadArtifacts.groovy
+++ downloadArtifacts.groovy
@@ -1,5 +1,12 @@
+groupId = "$groupId"
+artifactId = "$artifactId"
+repository = "$repository"
+nexusServer = "$nexusServer"
+fileType = "$fileType"
+version = "$version"
+
 new File("/home/student/Downloads/testtest.tar.gz").withOutputStream { out ->
-    def url = new URL("http://192.168.56.30:8081/repository/artifact-storage/com/epam/akonchyts/helloworld/34/helloworld-34.tar.gz").openConnection()
+    def url = new URL("${nexusServer}/repository/${repository}/${groupId}/${artifactId}/${version}/${ARTIFACT_NAME}").openConnection()
     def remoteAuth = "Basic " + "admin:admin123".bytes.encodeBase64()
     url.setRequestProperty("Authorization", remoteAuth);
     out << url.inputStream
