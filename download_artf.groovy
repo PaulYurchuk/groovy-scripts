9112bbf7ea33d9d0dbb4c69e0cdb0e41d99655c0
zvirinsky <virinsky@gmail.com>
2017-07-09 20:51:22 +0300

    pre-release

--- download_artf.groovy
+++ download_artf.groovy
@@ -1,14 +1,16 @@
+def user = "admin"
+def password = "admin123"
 def ARTIFACTID = "zvirinsky"
-def BUILD_NUMBER = "23"
+def BUILD_NUMBER = "22"
 def GROUPID = "Task-8"
 def VERSIONID = "1.0"
 def baseURL = "http://192.168.1.223:8081"
 def repositoryid = "MNT-maven2-hosted-releases"
-def asd = "tar.gz"
+String basicAuthString = "Basic " + "$user:$password".bytes.encodeBase64().toString()
 
 
-
-def url = "${baseURL}/repository/${repositoryid}/${GROUPID}/${ARTIFACTID}_${BUILD_NUMBER}/${VERSIONID}/"
-def file = new File("${ARTIFACTID}_${BUILD_NUMBER}-${VERSIONID}.${asd}").newOutputStream()
-file << new URL(url).openStream()
-file.close()
+new File("${ARTIFACTID}_${BUILD_NUMBER}-${VERSIONID}.tar.gz").withOutputStream { out ->
+    def url = new URL("${baseURL}/repository/${repositoryid}/${GROUPID}/${ARTIFACTID}_${BUILD_NUMBER}/${VERSIONID}/${ARTIFACTID}_${BUILD_NUMBER}-${VERSIONID}.tar.gz").openConnection()
+    url.setRequestProperty("Authorization", basicAuthString)
+    out << url.inputStream
+}
