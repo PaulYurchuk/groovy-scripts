7d80fa611eaca2430912681fb57d3ec3373beff0
Andrei Konchyts <a.konchic@gmail.com>
2017-07-09 19:32:37 +0300

    download artifacts version 1.0

--- downloadArtifacts.groovy
+++ downloadArtifacts.groovy
@@ -1,12 +1,21 @@
-groupId = "$groupId"
-artifactId = "$artifactId"
-repository = "$repository"
-nexusServer = "$nexusServer"
-fileType = "$fileType"
-version = "$version"
+CliBuilder cli = new CliBuilder(
+    usage: 'groovy test.groovy -a {ARTIFACT_NAME}')
+cli.with {
+  a longOpt: 'ARTIFACT_NAME', args: 1, required: true, 'ARTIFACT_NAME from job Jenkins'
+ }
+def options = cli.parse(args)
+if (!options) {
+  return
+}
 
-new File("/home/student/Downloads/testtest.tar.gz").withOutputStream { out ->
-    def url = new URL("${nexusServer}/repository/${repository}/${groupId}/${artifactId}/${version}/${ARTIFACT_NAME}").openConnection()
+def ARTIFACT_NAME = options.a
+def repository = "artifact-storage"
+def nexusServer = "http://192.168.56.30:8081"
+def ARTIFACT_SUFFIX = ARTIFACT_NAME.substring(0, ARTIFACT_NAME.lastIndexOf("-"))
+def BUILD_NUMBER = ARTIFACT_NAME.replaceAll("\\D+","")
+
+new File("${ARTIFACT_NAME}.tar.gz").withOutputStream { out ->
+    def url = new URL("${nexusServer}/repository/${repository}/${ARTIFACT_SUFFIX}/${ARTIFACT_SUFFIX}/${BUILD_NUMBER}/${ARTIFACT_NAME}.tar.gz").openConnection()
     def remoteAuth = "Basic " + "admin:admin123".bytes.encodeBase64()
     url.setRequestProperty("Authorization", remoteAuth);
     out << url.inputStream
