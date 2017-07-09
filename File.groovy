a2f13b94255d6325e452c6c765bd131093f2de8b
aion3181 <aion3181@gmail.com>
2017-07-09 22:14:09 +0300

    fixing

--- File.groovy
+++ File.groovy
@@ -51,11 +51,11 @@ def authInterceptor = new HttpRequestInterceptor() {
     }
 }
 
-def http = new HTTPBuilder($nexus)
+def http = new HTTPBuilder("$nexus")
 http.client.addRequestInterceptor(authInterceptor)
 
         if($choice=="push"){
-            File sourceFile = new File(filePath)
+            File sourceFile = new File("filePath")
             assert sourceFile.exists(): "${sourceFile} does not exist"
             println "pushing ${ARTIFACT_NAME}"
             http.request(PUT, 'application/octet-stream') { req ->
