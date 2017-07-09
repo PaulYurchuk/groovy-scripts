b30c800ac1c1ee8999aade2920889fb24fddb350
zvirinsky <virinsky@gmail.com>
2017-07-09 23:02:22 +0300

    switch to jenkins vars

--- nexus_files.groovy
+++ nexus_files.groovy
@@ -6,15 +6,15 @@ import static groovyx.net.http.Method.*
 
 def user = "admin"
 def password = "admin123"
-def ARTIFACTID = "zvirinsky"
-def BUILD_NUMBER = "21"
-def GROUPID = "Task-8"
+def ARTIFACTID = $ARTIFACTID
+def BUILD_NUMBER = $BUILD_NUMBER
+def GROUPID = $GROUPID
 def VERSIONID = "1.0"
-def baseURL = "http://192.168.1.223:8081"
-def repositoryid = "MNT-maven2-hosted-releases"
+def baseURL = "http://${NEXUSIP}:8081"
+def repositoryid = $REPOSITORYID
 def artifacts = []
 String basicAuthString = "Basic " + "$user:$password".bytes.encodeBase64().toString()
-def task = "pull" // "push"
+def task = $TASK
 def FILENAME = "${ARTIFACTID}_${BUILD_NUMBER}-${VERSIONID}.tar.gz"
 def FULLPATH = "${baseURL}/repository/${repositoryid}/${GROUPID}/${ARTIFACTID}_${BUILD_NUMBER}/${VERSIONID}/${ARTIFACTID}_${BUILD_NUMBER}-${VERSIONID}.tar.gz"
 def reqmap =  """ { "action": "coreui_Component", "method":"readAssets",
@@ -23,25 +23,25 @@ def reqmap =  """ { "action": "coreui_Component", "method":"readAssets",
                 "filter": [{"property":"repositoryName",
                 "value":"$repositoryid"}]}],
                 "type":"rpc", "tid":15 } """
+
 def reqartf = new HTTPBuilder("$baseURL")
+	reqartf.request(POST, TEXT) { req ->
+		headers."Authorization" = basicAuthString
+		uri.path = "/service/extdirect"
+		headers."Content-Type"="application/json"
+		body = reqmap
 
-reqartf.request(POST, TEXT) { req ->
-    headers."Authorization" = basicAuthString
-    uri.path = "/service/extdirect"
-    headers."Content-Type"="application/json"
-    body = reqmap
-
-    response.success = { resp, json ->
-        def slurper = new groovy.json.JsonSlurper()
-        def jsonT2S = json.text as String
-        def jsonParse = slurper.parseText(jsonT2S)
-        jsonParse.result.data.each {
-            if (it.name.matches(~/.+.tar.gz/)) {
-                artifacts.add(it.name)
-            }
-        }
-    }
-}
+		response.success = { resp, json ->
+			def slurper = new groovy.json.JsonSlurper()
+			def jsonT2S = json.text as String
+			def jsonParse = slurper.parseText(jsonT2S)
+			jsonParse.result.data.each {
+				if (it.name.matches(~/.+.tar.gz/)) {
+					artifacts.add(it.name)
+				}
+			}
+		}
+	}
 
 for ( i in artifacts ) {
     println i
