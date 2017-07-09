89e3307993b3b660a52d726fca8b36d6f04eb3af
bubalush <bubalush@gmail.com>
2017-07-09 18:54:34 +0300

    Update sftp.groovy

--- sftp.groovy
+++ sftp.groovy
@@ -1,5 +1,19 @@
 @Grab(group='com.jcraft', module='jsch', version='0.1.46')
 import com.jcraft.jsch.*
+    
+CliBuilder cli = new CliBuilder(
+    usage: 'groovy sftp.groovy -a {ARTIFACT_NAME} -p {WORKSPACE}') 
+cli.with {
+  a longOpt: 'ARTIFACT_NMAE', args: 1, required: true, 'Artifact_name from job Jenkins'
+  p longOpt: 'WORKSPACE', args: 1, required: true, 'ЦЩКЛЫЗФСУ from job Jenkins'
+ }
+def options = cli.parse(args)
+if (!options) {
+  return
+}
+
+def  ARTIFACT_NAME= options.a
+def WORKSPACE= options.p
 
 java.util.Properties config = new java.util.Properties()
 config.put "StrictHostKeyChecking", "no"
@@ -12,7 +26,7 @@ sess.with {
     Channel chan = openChannel "sftp"
     chan.connect()
     ChannelSftp sftp = (ChannelSftp) chan;
-    def sessionsFile = new File('/opt/jenkins/master/workspace/Task8test/artifact.tar.gz')
+    def sessionsFile = new File('${WORKSPACE}/$ARTIFACT_NAME')
     sessionsFile.withInputStream { istream -> sftp.put(istream, "/home/artifact.tar.gz") }
     chan.disconnect()
     disconnect()
