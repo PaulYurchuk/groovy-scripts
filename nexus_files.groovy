76031b0d7a3909498e57d0508fd4dbdf0a3eda6f
Zakhar Virinsky <virinsky@gmail.com>
2017-07-10 10:54:47 +0300

    add cli builder

--- nexus_files.groovy
+++ nexus_files.groovy
@@ -12,9 +12,21 @@ def baseURL = "http://10.6.102.119:8081"
 def REPOSITORYID = "MNT-maven2-hosted-releases"
 def artifacts = []
 String basicAuthString = "Basic " + "$user:$password".bytes.encodeBase64().toString()
-def TASK = $TASK
 
-if (TASK == "pull") {
+CliBuilder cli = new CliBuilder(
+   usage: 'groovy nexus_files.groovy -p {TASK ("pull/push")}')
+ cli.with {
+   p longOpt: 'TASK', args: 1, required: true, values: ['pull','push'], 'What to do with artifact?'
+ }
+def options = cli.parse(args)
+if (!options) {
+  return
+}
+
+def TASK = options.p
+
+
+if ("$TASK" == "pull") {
         new File("${ARTIFACTID}-${VERSIONID}.tar.gz").withOutputStream { out ->
             def url = new URL("${baseURL}/repository/${repositoryid}/${GROUPID}/${ARTIFACTID}/${VERSIONID}/${ARTIFACTID}-${VERSIONID}.tar.gz").openConnection()
             url.setRequestProperty("Authorization", basicAuthString)
