b24d9c6b67ba8d3815036c3c2378b2b5c2369d49
bubalush <bubalush@gmail.com>
2017-07-10 11:43:56 +0300

    Update pull-push.groovy

--- pull-push.groovy
+++ pull-push.groovy
@@ -1,10 +1,9 @@
 CliBuilder cli = new CliBuilder(
-    usage: 'groovy upload.groovy -p {PULLPUSH}  -a {ARTIFACT_NAME} -b {ARTIFACT_SUFFIX} -c {BUILD_NUMBER}')
+    usage: 'groovy upload.groovy -p {PULLPUSH}  -a {ARTIFACT_NAME} ')
 cli.with {
    p longOpt: 'PULLPUSH', args: 1, required: true, values: ['pull','push'], 'Choose pull or push artifact'
    a longOpt: 'ARTIFACT_NAME', args: 1, 'ARTIFACT_NAME from job Jenkins'
-   b longOpt: 'ARTIFACT_SUFFIX', args: 1, 'ARTIFACT_SUFFIX from job Jenkins'
-   c longOpt: 'BUILD_NUMBER', args: 1, 'BUILD_NUMBER from job Jenkins'
+   
  }
 def options = cli.parse(args)
 if (!options) {
@@ -12,8 +11,6 @@ if (!options) {
 }
 
 def  ARTIFACT_NAME= options.a
-def  ARTIFACT_SUFFIX= options.b
-def BUILD_NUMBER= options.c
 def PULLPUSH = options.p
 
 def cred = "nexus-service-user:jenkins"
@@ -23,6 +20,8 @@ def way = "http://192.168.50.11:8081"
 if("$PULLPUSH"=="pull"){
 
 println "pull ${ARTIFACT_NAME}"
+def ARTIFACT_SUFFIX = ARTIFACT_NAME.substring(0, ARTIFACT_NAME.lastIndexOf("-"))     
+def BUILD_NUMBER = ARTIFACT_NAME.replaceAll("\\D+","")
 def File = new File ("scripts/${ARTIFACT_SUFFIX}-${BUILD_NUMBER}.tar.gz").getBytes()
 def connection = new URL( "${way}/repository/${repo}/${ARTIFACT_SUFFIX}/${ARTIFACT_SUFFIX}/${BUILD_NUMBER}/${ARTIFACT_SUFFIX}-${BUILD_NUMBER}.tar.gz" )
         .openConnection() as HttpURLConnection
@@ -40,9 +39,9 @@ println connection.responseCode
 }
 else {
       println "push ${ARTIFACT_NAME}"
-      def ARTIFACT_SUFFIX = ARTIFACT_NAME.substring(0, ARTIFACT_NAME.lastIndexOf("-"))     
+     def ARTIFACT_SUFFIX = ARTIFACT_NAME.substring(0, ARTIFACT_NAME.lastIndexOf("-"))     
       def BUILD_NUMBER = ARTIFACT_NAME.replaceAll("\\D+","")
-new File("$ARTIFACT_NAME").withOutputStream { out ->
+     new File("$ARTIFACT_NAME").withOutputStream { out ->
     def url = new URL("${way}/repository/${repo}/${ARTIFACT_SUFFIX}/${ARTIFACT_SUFFIX}/${BUILD_NUMBER}/${ARTIFACT_NAME}").openConnection()
     def remoteAuth = "Basic " + "${cred}".bytes.encodeBase64()
     url.setRequestProperty("Authorization", remoteAuth);
