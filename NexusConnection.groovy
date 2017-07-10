fbfd0b7b1466c508aaf9150948f578c23ed71168
Anton Maslakov <antoxam1997@gmail.com>
2017-07-10 17:03:04 +0300

    list artifact

---
+++ NexusConnection.groovy
@@ -0,0 +1,42 @@
+@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.2')
+import groovyx.net.http.HTTPBuilder
+
+import org.apache.http.HttpRequest
+import org.apache.http.HttpRequestInterceptor
+import org.apache.http.protocol.HttpContext
+
+import static groovy.io.FileType.FILES
+import static groovyx.net.http.ContentType.TEXT
+import static groovyx.net.http.Method.PUT
+
+def filePath = "/Users/dart/workspace/artifact-maven-73-1.0.tar.gz"
+def repName = "my-maven-storage"
+def nexusURL = "1.1.0.33:8081/"
+def nexusLogin = "admin"
+def nexusPass = "admin123"
+def group = "/artifacts/artifact-maven-73/1.0"
+def artifact = "artifact-maven-73-1.0.tar.gz"
+def vesion = "1.0"
+
+
+File sourceFile = new File(filePath)
+assert sourceFile.exists(): "${sourceFile} does not exist"
+def authInterceptor = new HttpRequestInterceptor() {
+    void process(HttpRequest httpRequest, HttpContext httpContext) {
+        httpRequest.addHeader('Authorization', 'Basic ' + "${nexusLogin}:${nexusPass}".bytes.encodeBase64().toString())
+    }
+}
+println "pushing ${sourceFile.name}"
+def http = new HTTPBuilder( "http://${nexusURL}" )
+http.client.addRequestInterceptor(authInterceptor)
+http.request( PUT, 'application/octet-stream' ) { req ->
+    uri.path = "/repository/${repName}/${group}/${artifact}/${vesion}/${artifact}-${vesion}.tar.gz"
+    headers."Content-Type"="application/octet-stream"
+    headers."Accept"="*/*"
+    body = sourceFile.bytes
+    response.success = { resp ->
+        println "POST response status: ${resp.statusLine}"
+        assert resp.statusLine.statusCode == 201
+    }
+
+}
---
+++ ShowArtifacts.groovy
@@ -0,0 +1,42 @@
+
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1' )
+import groovyx.net.http.HTTPBuilder
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
+def listArtifacts = []
+def httpreq =  """ { "action": "coreui_Component",
+    "method":"readAssets",
+    "data":[{"page":"1", "start":"0",
+    "limit":"300", "sort":[{"property":"name","direction":"ASC"}],
+    "filter":[{"property":"repositoryName","value":"my-maven-storage"}]}],
+    "type":"rpc",
+    "tid":15
+	} """
+
+def remote = new HTTPBuilder("http://1.1.0.33:8081/")
+
+remote.request(POST, TEXT) { req ->
+    uri.path = "/service/extdirect"
+    headers."Content-Type" = "application/json"
+    headers.'Accept' = "*/*"
+
+    body = httpreq
+
+    headers.'Authorization' =
+            "Basic ${"admin:admin123".bytes.encodeBase64().toString()}"
+
+    response.success = { resp, json ->
+        println "Success! ${resp.status}"
+
+        def slurper = new groovy.json.JsonSlurper()
+        def jsonRes = json.text as String
+        def parsed = slurper.parseText(jsonRes)
+
+        parsed.result.data.each {
+            if (it.name.matches(~/.+.tar.gz/)) {
+                listArtifacts.add(it.name)
+            }
+        }
+    }
+}
+println(listArtifacts)
