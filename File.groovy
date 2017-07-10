f630737e21e765671c6158f67b32f8e70a8241a5
aion3181 <aion3181@gmail.com>
2017-07-10 15:07:00 +0300

    Update File.groovy

--- File.groovy
+++ File.groovy
@@ -1,4 +1,4 @@
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.2' )
+@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.2')
 
 import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.ContentType.*
