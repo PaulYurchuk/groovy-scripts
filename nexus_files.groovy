e76ecba3801eadd50900e38126baf54dbf4dde09
Zakhar Virinsky <virinsky@gmail.com>
2017-07-10 13:07:31 +0300

    minor changes

--- nexus_files.groovy
+++ nexus_files.groovy
@@ -4,22 +4,22 @@ import groovyx.net.http.HTTPBuilder
 import static groovyx.net.http.ContentType.*
 import static groovyx.net.http.Method.*
 
-def user = "admin"
-def password = "admin123"
+def user = "nexus-service-user"
+def password = "nexus-service-user"
 def GROUPID = "Task-8"
-def VERSIONID = "1.0"
 def baseURL = "http://10.6.102.18:8081"
 def artifacts = []
 String basicAuthString = "Basic " + "$user:$password".bytes.encodeBase64().toString()
 
 CliBuilder cli = new CliBuilder(
-   usage: 'groovy nexus_files.groovy -t {TASK ("pull/push")} -a -b -c -r')
+   usage: 'groovy nexus_files.groovy -t {TASK ("pull/push")} -a -b -c -r -g')
  cli.with {
    t longOpt: 'TASK', args: 1, required: true, values: ['pull','push'], 'What to do with artifact?'
    a longOpt: 'ARTIFACTID', args: 1, 'ARTIFACTID'
    b longOpt: 'ARTIFACT_SUFFIX', args: 1, 'ARTIFACT_SUFFIX'
    c longOpt: 'BUILD_NUMBER', args: 1, 'BUILD_NUMBER'
    r longOpt: 'REPOSITORYID', args: 1, 'REPOSITORYID'
+   g longOpt: 'REPOSITORYID', args: 1, 'REPOSITORYID'
  }
 def options = cli.parse(args)
 if (!options) {
@@ -31,6 +31,8 @@ def ARTIFACTID = options.a
 def ARTIFACT_SUFFIX = options.b
 def BUILD_NUMBER = options.c
 def REPOSITORYID = options.r
+def GROUPID = options.g
+def VERSIONID = ${BUILD_NUMBER}
 
 
 
