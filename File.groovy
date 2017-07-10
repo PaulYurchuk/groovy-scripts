6448bc9c9a48248878791c627a9f6c78f62cf198
aion3181 <aion3181@gmail.com>
2017-07-10 11:30:47 +0300

    Update File.groovy

--- File.groovy
+++ File.groovy
@@ -60,10 +60,10 @@ http.client.addRequestInterceptor(authInterceptor)
             def File = new File (filePath)
             assert File.exists(): "${File} does not exist"
             http.request(PUT, 'application/octet-stream') { req ->
-                uri.path = "/repository/${repo}/${groupID}/${artifactID}/${Version}/${ARTIFACT_NAME}"
+                    uri.path = "/repository/${repo}/${groupID}/${artifactID}/${Version}/${artifactID}-${Version}.tar.gz"
                 headers."Content-Type"="application/octet-stream"
                 headers."Accept"="*/*"
-                body = File.getBytes()
+                body = File.bytes
                 response.success = { resp ->
                     println "POST response status: ${resp.statusLine}"
                     assert resp.statusLine.statusCode == 201
