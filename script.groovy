379ed913eb288ab877d8d5a5c763a4c23a9976eb
bubalush <bubalush@gmail.com>
2017-07-10 09:30:23 +0300

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,8 +1,7 @@
-//Get list of Artifacts from Nexus3 to Jenkins
-def nexusURL = "http://nexus"
-def repo = "task8"
-def cred = "admin:admin123"
-def query = """ { "action": "coreui_Component",    "method":"readAssets",    "data":[{"page":"1", "start":"0",    "limit":"300", "sort":[{"property":"name","direction":"ASC"}],    "filter":[{"property":"repositoryName","value":"${repo}"]}],    "type":"rpc",    "tid":15	} """
+def nexusURL = "http://10.6.102.41:8081"
+def repo = "project-releases"
+def cred = "nexus-service-user:jenkins"
+def query = """ { "action": "coreui_Component",    "method":"readAssets",    "data":[{"page":"1", "start":"0",    "limit":"300", "sort":[{"property":"name","direction":"ASC"}],    "filter":[{"property":"repositoryName","value":"${repo}"}]}],    "type":"rpc",    "tid":15	} """
 def connection = new URL( "${nexusURL}/service/extdirect" )
         .openConnection() as HttpURLConnection
 def auth = "${cred}".getBytes().encodeBase64().toString()
@@ -21,17 +20,18 @@ writer.write(query)
 writer.flush()
 writer.close()
 connection.connect()
-
 //parser Response from Nexus3
 def listArtifacts=[]
+
 def slurper = new groovy.json.JsonSlurper()
 def response = connection.inputStream.text
 def parsed = slurper.parseText(response)
+
 parsed.result.data.each {
-    if (it.name.matches(~/.+.tar.gz/)) {
+       if (it.name.matches(~/.+.tar.gz/)) {
         def cutStr = it.name.substring(it.name.lastIndexOf("/")+1 , it.name.length())
         listArtifacts.add(cutStr)
-            }
+       
+           }
 }
-
-listArtifacts.reverse()
+listArtifacts
