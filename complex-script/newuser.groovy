2b0c6f99a9fe47a523ba6046dd17f4739b1aa85b
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-10 12:50:29 +0300

    First commit

--- complex-script/newuser.groovy
+++
@@ -1,8 +0,0 @@
-import groovy.json.JsonOutput
-
-// Create new admin user
-//
-def deploy = ['nx-deploy']
-def Alex = security.addUser('AlexK', 'Alex', 'K', 'aliaksei_k@epam.com', true, 'ak123', deploy)
-log.info('User jane.doe created')
-
