d8a6f03be6fbe6b0ee066387264978e7d1cd278d
aion3181 <aion3181@gmail.com>
2017-07-11 16:02:51 +0300

    Update pullsh.groovy

--- pullsh.groovy
+++ pullsh.groovy
@@ -70,7 +70,7 @@ else {
     "tid":15
     } """
 
-               http.client.addRequestInterceptor(authInterceptor)
+               http.auth.basic username, password
                http.request(POST, TEXT) { req ->
                uri.path = '/service/extdirect'
                headers."Content-Type" = "application/json"
