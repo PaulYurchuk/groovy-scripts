a4caa06bc4bfc894dd5b0d345eccbcef577ced49
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-11 16:36:48 +0300

    Fix exec ssh script

--- jobs.groovy
+++ jobs.groovy
@@ -158,7 +158,7 @@ job (deployer){
                     pathToKey('/opt/jenkins/master/id_rsa')
                 }
                 transferSet {
-                    execCommand('if [-e /opt/tomcat/webapps/helloworld.war] then mv /opt/tomcat/webapps/helloworld.war /opt/tomcat/webapps/helloworld.war.old fi && rm -rf /opt/tomcat/webapps/helloworld.war')
+                    execCommand('''if [-e '/opt/tomcat/webapps/helloworld.war'] then mv /opt/tomcat/webapps/helloworld.war /opt/tomcat/webapps/helloworld.war.old fi && rm -rf /opt/tomcat/webapps/helloworld.war''')
                 }
             }
         }
