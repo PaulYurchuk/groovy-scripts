ffb1ff4884627da694b4347f5d3c5b3d543d80b2
Vadzim Tarasiuk <Vadzim_Tarasiuk@epam.com>
2017-07-11 11:56:10 +0300

    Created skel for dsl job-creator

---
+++ jobs.groovy
@@ -0,0 +1,63 @@
+def gitrepo1 = 'MNT-Lab/groovy-scripts'
+def gitrepo2 = 'MNT-Lab/build-principals'
+def gitrepoforked = 'VadzimTarasiuk/jboss-eap-quickstarts'
+def versionbranch = '7.1.0.Beta'
+def branchname = 'vtarasiuk'
+
+/** Name Section **/
+
+/** Setting master-job name*/
+def lord = 'MNT-CD-module9-build-job'
+
+/** Setting list of child job names  (hardcode)*/
+
+
+/**Job Section**/
+
+/** Create Master job*/
+mavenJob("${lord}") {
+    parameters {
+        parameters {
+            stringParam('ARTIFACT_NAME', 'helloworld-$BUILD_NUMBER', 'Artifact name: suffix + build Number')
+        }
+    }
+    multiscm {
+        git {
+            github (gitrepo1, branchname)
+            extensions {
+                cloneOptions {
+                    reference ('$WORKSPACE/groovy')
+                }
+            }
+        }
+        git {
+            github (gitrepo2, branchname)
+            extensions {
+                cloneOptions {
+                    reference ('$WORKSPACE/buildp')
+                }
+            }
+        }
+        git {
+            github (gitrepoforked, versionbranch)
+            extensions {
+                cloneOptions {
+                    reference ('$WORKSPACE/jboss-eap')
+                }
+            }
+        }
+    }
+    preBuildSteps {
+        shell ("jboss-eap/helloworld/autofill.sh")
+    }
+    rootPOM ('jboss-eap/helloworld/pom.xml')
+    goals ('clean install -DskipTests')
+    postBuildSteps ('SUCCESS') {
+        shell('tar -zcvf $ARTIFACT_NAME.tar.gz -C jboss-eap/helloworld/target/ helloworld.war && cp $ARTIFACT_NAME.tar.gz ./scripts/')
+        groovyScriptFile ('pull-push.groovy -p push -a $ARTIFACT_NAME')
+    }
+    publishers {
+        archiveArtifacts('*.tar.gz')
+    }
+}
+
