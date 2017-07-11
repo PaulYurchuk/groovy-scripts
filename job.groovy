1e550f2dd787cf948243e2c590cec315674cb51d
Yuri Shchanouski <student@epbyminw2468.minsk.epam.com>
2017-07-11 11:17:44 +0300

    Add groovy files

---
+++ job.groovy
@@ -0,0 +1,25 @@
+def git = "MNT-Lab/groovy-scripts"
+def repo = "yshchanouski"
+
+job("EPBYMINW2468/MNT-CD-module9-extcreated-job") {
+    parameters {
+        stringParam('BRANCH_NAME', 'repo')
+    }
+    scm {
+	git {
+		remote {
+		        github(git, '$BRANCH_NAME')
+		}
+		extensions {
+			cleanBeforeCheckout()
+		}
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    steps {
+	shell('echo "Hello"')
+	}
+    }
+}
+
