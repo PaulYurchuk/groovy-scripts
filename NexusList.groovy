22d17ad95ea0b66fbe44ca52e7a67e4a6f8c5b57
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-11 16:44:45 +0300

    artifact naming for pull from Nexus

--- NexusList.groovy
+++ NexusList.groovy
@@ -33,7 +33,7 @@ remote.request(POST, TEXT) { req ->
 
         parsed.result.data.each {
             if (it.name.matches(~/.+.tar.gz/)){
-                def myparser = it.name.substring(it.name.lastIndexOf("/")+1 , it.name.lastIndexOf("-")+5)
+                def myparser = it.name.substring(it.name.lastIndexOf("/")+1 , it.name.length())
                 listArtifacts.add(myparser)
             }
         }
