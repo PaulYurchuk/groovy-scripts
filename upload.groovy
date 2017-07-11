8327f46519f9dfd966650cd2dd14a60e7713aa99
marijademenkova <mar.demenkova@gmail.com>
2017-07-11 11:21:29 +0300

    Update upload.groovy

--- upload.groovy
+++ upload.groovy
@@ -1,55 +1,40 @@
-import groovyx.net.http.Method
-import groovyx.net.http.ContentType;
+@Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.2')
+import groovyx.net.http.HTTPBuilder
 import org.apache.http.HttpRequest
 import org.apache.http.HttpRequestInterceptor
-import org.apache.http.entity.mime.MultipartEntity
-import org.apache.http.entity.mime.content.FileBody
-import org.apache.http.entity.mime.content.StringBody
 import org.apache.http.protocol.HttpContext
-import groovyx.net.http.HttpResponseException;
+
+import static groovy.io.FileType.FILES
+import static groovyx.net.http.ContentType.TEXT
+import static groovyx.net.http.Method.PUT
+
+def filePath = "/opt/jenkins/master/artifacts/hello-28.tar.gz"
+def repName = "artifact"
+def nexusLogin = "admin"
+def nexusPass = "admin123"
+def group = "hello"
+def artifact = "hello_28"
+def version = "1.0"
 
 
-def groupid="34"
-def buildversion="34"
-def artifactname="helloworld-34"
-
-http://192.168.56.51:8081/repository/artifact/helloworld-34.tar.gz
-
-
-
-class NexusUpload {
-  def uploadArtifact(Map artifact, File fileToUpload, String user, String password) {
-    def path = "/repository/artifact/${groupid}/${buildversion}/${artifactname}/${buildversion}-${artifactname}.tar.gz"
-    HTTPBuilder http = new HTTPBuilder("http://192.168.56.51:8081")
-    String basicAuthString = "Basic " + "admin:admin123".bytes.encodeBase64().toString()
-
-    http.client.addRequestInterceptor(new HttpRequestInterceptor() {
-      void process(HttpRequest httpRequest, HttpContext httpContext) {
-        httpRequest.addHeader('Authorization', basicAuthString)
-      }
-    })
-    try {
-      http.request(Method.POST, ContentType.ANY) { req ->
-        uri.path = path
-
-      MultipartEntity entity = new MultipartEntity()
-      entity.addPart("hasPom", new StringBody("false"))
-      entity.addPart("file", new FileBody(fileToUpload))
-      entity.addPart("a", new StringBody("my-artifact-id"))
-      entity.addPart("g", new StringBody("my-group-id"))
-      entity.addPart("r", new StringBody("my-repository"))
-      entity.addPart("v", new StringBody("my-version"))
-      req.entity = entity
-
-      response.success = { resp, reader ->
-        if(resp.status == 201) {
-          println "success!"
-        }
-      }
+File sourceFile = new File(filePath)
+assert sourceFile.exists(): "${sourceFile} does not exist"
+def authInterceptor = new HttpRequestInterceptor() {
+    void process(HttpRequest httpRequest, HttpContext httpContext) {
+        httpRequest.addHeader('Authorization', 'Basic ' + "${nexusLogin}:${nexusPass}".bytes.encodeBase64().toString())
+    }
+}
+println "pushing ${sourceFile.name}"
+def http = new HTTPBuilder( "http://192.168.56.51:8081" )
+http.client.addRequestInterceptor(authInterceptor)
+http.request( PUT, 'application/octet-stream' ) { req ->
+    uri.path = "/repository/${repName}/${group}/${artifact}/${version}/${artifact}-${version}.tar.gz"
+    headers."Content-Type"="application/octet-stream"
+    headers."Accept"="*/*"
+    body = sourceFile.bytes
+    response.success = { resp ->
+        println "POST response status: ${resp.statusLine}"
+        assert resp.statusLine.statusCode == 201
     }
 
-    } catch (HttpResponseException e) {
-      e.printStackTrace()
-    }
-  }
 }
